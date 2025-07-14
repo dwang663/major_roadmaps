b643e5c12c30b169cc06dd18a7ad19cafeb7ecaf
Daniel Wang <daniel@Daniels-MBP-8.attlocal.net>
2025-07-14 15:09:46 -0700

    updated json to truncate at 50 words. updated afam to dynamically pull data from github page

--- data/clean.py
+++ data/clean.py
@@ -1,9 +1,8 @@
 import json
 import re
 
-with open("./data/courses.json", "r", encoding="utf-8") as f:
-    text = json.load(f)
-  
+WORD_LIMIT = 50
+
 def clean_text(value):
     value = re.sub(r'\\u[0-9a-fA-F]{4}', '', value)
     value = value.replace('\\', '')
@@ -13,13 +12,23 @@ def clean_text(value):
     value = value.replace('"', '\\"')
     return value
 
+def truncate_to_n_words(text, n):
+    words = text.split()
+    if len(words) > n:
+        return ' '.join(words[:n]) + '...'
+    return text
+
+with open("courses.json", "r", encoding="utf-8") as f:
+    text = json.load(f)
+
 code_to_description = {}
 
 for course in text["data"]["courses"]:
     description = clean_text(course.get("description", ""))
+    description = truncate_to_n_words(description, WORD_LIMIT)
     for listing in course.get("listings", []):
         course_code = clean_text(listing.get("course_code", ""))
         code_to_description[course_code] = description
 
-with open("./data/cleaned_course_data.json", "w", encoding="utf-8") as f:
+with open("cleaned_course_data.json", "w", encoding="utf-8") as f:
     json.dump(code_to_description, f, indent=2, ensure_ascii=False)
